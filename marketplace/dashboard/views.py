cf4247139fbb8d7ec9766eb10d67c2bbc06f10bf
Leon --Remote <leon@entwurf.co.uk>
2023-11-08 11:08:18 +0000

    created an editItem view and form

--- marketplace/dashboard/views.py
+++ marketplace/dashboard/views.py
@@ -10,3 +10,9 @@ def index(request):
     return render(request, 'dashboard/index.html', {
         'items': items,
     })
+
+
+@login_required
+def delete(request, pk):
+    item = get_object_or_404(Item, pk=pk, created_by=request.user)
+    item.delete
